383f8bef4ea7c6cb5f2f83200182f82b9f73263f
lisa-nakanishi <iwarehiko3@gmail.com>
2021-04-25 19:59:31 +0900

    Initial commit

---
+++ Assets/Score.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    //スコアを表示するテキスト
+    public GameObject ScoreText;
+    //スコアを初期化
+    private int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        this.ScoreText = GameObject.Find("ScoreText");
+       
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        string Tag = collision.gameObject.tag;
+        //
+        if(Tag== "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (Tag == "LargeStarTag")
+        {
+            score += 20;
+        }
+        else if (Tag == "SmallCloudTag")
+        {
+            score += 15;
+        }
+        else if (Tag == "LargeCloudTag")
+        {
+            score += 30;
+        }
+        
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ScoreTextにスコアを表示
+        this.ScoreText.GetComponent<Text>().text = "Score:" + score.ToString();
+    }
+}
